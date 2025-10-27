4f66663e593fd5bace623cd1277258c4db277ebc
Zorg <zorgiepoo@gmail.com>
2025-10-26 21:38:38 -0700

    Add missing executable bit permission warnings
    These warnings are logged after failing to connect to XPC and launchd helpers and indicate the helpers likely did not launch at all.
    
    This error happens when developers incorrectly copy Sparkle framework not preserving executable permissions, and is sadly somewhat common.

--- Sparkle/SPUXPCServiceInfo.m
+++ Sparkle/SPUXPCServiceInfo.m
@@ -20,3 +20,36 @@ BOOL SPUXPCServiceIsEnabled(NSString *enabledKey)
     
     return [mainBundleHost boolForInfoDictionaryKey:enabledKey];
 }
+
+BOOL SPUHelperHasExecutablePermission(NSString *component, NSString * _Nullable __autoreleasing * _Nullable failureReason)
+{
+    NSBundle *sparkleBundle = [NSBundle bundleWithIdentifier:SUBundleIdentifier];
+    NSURL *helperURL = [[sparkleBundle.bundleURL URLByAppendingPathComponent:component isDirectory:NO] URLByResolvingSymlinksInPath];
+    
+    NSNumber *isExecutableFile = nil;
+    NSError *getResourceError = nil;
+    if (![helperURL getResourceValue:&isExecutableFile forKey:NSURLIsExecutableKey error:&getResourceError]) {
+        if (failureReason != NULL) {
+            *failureReason = [NSString stringWithFormat:@"Failed to fetch info from file '%@' -- does this helper exist? %@", helperURL.path, getResourceError.localizedDescription];
+        }
+        
+        return NO;
+    }
+    
+    if (!isExecutableFile.boolValue) {
+        if (failureReason != NULL) {
+            *failureReason = [NSString stringWithFormat:@"The file '%@' is not considered an executable -- were the executable permissions lost during a bad file copy? Please ensure file permissions and symbolic links for Sparkle framework are preserved.", helperURL.path];
+        }
+        
+        return NO;
+    }
+    
+    return YES;
+}
+
+BOOL SPUXPCServiceHasExecutablePermission(NSString *serviceName, NSString * _Nullable __autoreleasing * _Nullable failureReason)
+{
+    NSString *componentName = [NSString stringWithFormat:@"XPCServices/%@.xpc/Contents/MacOS/%@", serviceName, serviceName];
+    
+    return SPUHelperHasExecutablePermission(componentName, failureReason);
+}
